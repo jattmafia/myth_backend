349f5b77483ecc93debbbb26a1ef59076f6299a4
jassi <jaspalhothi100@gmail.com>
2025-04-25 22:10:44 +0530

    changes

--- src/models/user.js
+++ src/models/user.js
@@ -28,7 +28,6 @@ const UserSchema = new mongoose.Schema({
   // Confirm password will not be saved, just for validation during signup
   confirmPassword: {
     type: String,
-    required: true,
   }
 });
 
